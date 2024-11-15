0fa0f5fd9c7e91d1bdaa0108559cd26e3e3b9863
Guilherme Mendonça Pinheiro <guipinheirobauru9072@gmail.com>
2024-11-15 17:32:07 -0300

    Changes

---
+++ My project/Assets/Scripts/Movimento Jogador.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoJogador : MonoBehaviour
+{
+    private CharacterController controller;
+    private Transform myCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        myCamera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movimento = new Vector3(horizontal, 0, vertical);
+
+        movimento = myCamera.TransformDirection(movimento);
+        movimento.y = 0;
+
+        controller.Move(movimento * Time.deltaTime * 5);
+        controller.Move(new Vector3(0, -9.81f, 0) * Time.deltaTime);
+
+        if(movimento != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movimento), Time.deltaTime * 10);
+        }
+        
+    }
+}
